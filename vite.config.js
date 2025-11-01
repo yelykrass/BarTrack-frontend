ab8b3399ca9bdf5d74cfafabf19af16575b2fd08
Yelyzaveta Krasiuk <lizakrasyuk24@gmail.com>
2025-11-01 23:33:09 +0100

    add certs https

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync("./certs/key.pem"),
+      cert: fs.readFileSync("./certs/cert.pem")
+    },
+    port: 5173
+  }
+});
+
